4fc6842645345eef4dbd36ddc62c7def2a9495dd
Antonios Pavlakis <antonios.pavlakis@staysafeapp.com>
2019-03-04 10:22:07 +0000

    Upgrade to docker-compose 1.23.2

--- ci/scripts/install-docker-compose.sh
+++ ci/scripts/install-docker-compose.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env sh
 
-set -eu
-
-# Add python pip and bash
-apk add --no-cache py-pip bash
-
-# Install docker-compose via pip
-pip install --no-cache-dir docker-compose
-docker-compose -v
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-Linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
